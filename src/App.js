7295d1960e4d5a2b6b2b45ca3d10ebbb80c2807e
Lazzatt04 <lazzat31032004@gmail.com>
2024-10-18 02:19:49 +0500

    Ecom_2 #1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import './App.css';
+  import './App.css';
 import Navigation from "./custumer/components/Navigation/Navigation";
 import HomePage from "./custumer/pages/HomePage/HomePage";
 import Footer from "./custumer/components/Footer/Footer";
+  import Product from "./custumer/components/Product/Product";
 
 function App() {
     return (
         <div className="">
             <Navigation/>
             <div>
-            <HomePage/>
+                {/* <HomePage/> */}
+                <Product/>
             </div>
             <div>
             <Footer/>
